501de474cc86379dfc68ec92fa9ae3ab0d3f6d3d
victor dedieu <louis.victordedieu@gmail.com>
2023-03-21 18:47:34 +0100

    feat: ajout des routes pour relier la page accueil Ref #3

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {AccueilComponent} from './accueil/accueil.component'
-import { AccueilQuizzComponent } from './quizz/accueil-quizz/accueil-quizz.component';
+import {ConfigurationComponent} from "./config/configuration/configuration.component";
+import {MonProfilComponent} from "./mon-profil/mon-profil.component";
+import {ListesQuizzComponent} from "./quizz/listes-quizz/listes-quizz.component";
 
 
 const routes: Routes = [
   { path: '',redirectTo:'/accueil', pathMatch:'full'},
   { path: 'accueil', component: AccueilComponent },
-  { path: 'accueil-quizz', component: AccueilQuizzComponent}
-
-
+  { path: 'configuration', component: ConfigurationComponent},
+  { path: 'mon-profil', component: MonProfilComponent},
+  { path: 'statistiques', component: MonProfilComponent},
+  { path: 'liste-quizz', component: ListesQuizzComponent}
 ];
 
 @NgModule({
