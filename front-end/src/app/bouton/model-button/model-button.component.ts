8f2a6fcc0f7d5fcb701716697f797882a4fedf08
victor dedieu <louis.victordedieu@gmail.com>
2023-03-22 09:32:02 +0100

    feat: creation d'une section bouton et du 1er component bouton Ref #3

---
+++ front-end/src/app/bouton/model-button/model-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-button',
+  templateUrl: './model-button.component.html',
+  styleUrls: ['./model-button.component.scss']
+})
+export class ModelButtonComponent {
+
+}
