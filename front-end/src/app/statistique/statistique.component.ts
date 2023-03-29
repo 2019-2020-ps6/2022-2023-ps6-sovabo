e0ab139d6354fa17931189897b9f1dab70a2430e
Léo Mendes Melin <leo.mendes-melin@hotmail.fr>
2023-03-29 22:53:11 +0200

    feat : avancement de la page stat avec la template Ref #18

--- front-end/src/app/statistique/statistique.component.ts
+++ front-end/src/app/statistique/statistique.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Quiz } from '../../models/quizz.model';
+import { QuizService } from '../../service/quizz.service';
+
 
 @Component({
   selector: 'app-statistique',
   templateUrl: './statistique.component.html',
-  styleUrls: ['./statistique.component.scss']
+  styleUrls: ['./statistique.component.scss',"../../assets/css/material-dashboard.css"]
 })
 export class StatistiqueComponent {
 
+
+  public quiz!: Quiz;
+
+  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+  }
+
+  ngOnInit() {
+    this.quiz = this.quizService.getQuizCourant();
+  }
 }
