affbc241bca76db9b6cf801c31561401447daad4
Julien ORIOL <julien@MacBook-Pro-de-Julien.local>
2023-05-07 21:16:08 +0200

    refactor: reimplémentation de l'animateur dans la page resultatQuiz avec animations Ref #5

--- front-end/src/app/quizz/resultat-quizz/resultat-quizz.component.ts
+++ front-end/src/app/quizz/resultat-quizz/resultat-quizz.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Quiz } from '../../../models/quizz.model';
 import { QuizService } from '../../../service/quizz.service';
+import {AnimateurService} from "../../../service/animateur.service";
 
 
 @Component({
@@ -14,12 +15,16 @@ export class ResultatQuizzComponent {
   score!: number;
   public quiz!: Quiz;
 
-  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+  constructor(private route: ActivatedRoute, private quizService: QuizService, private animateurService: AnimateurService) {
   }
 
   ngOnInit() {
     this.quiz = this.quizService.getQuizCourant();
     this.score = this.quizService.getScore();
+  }
+
+  getAnimateur() {
+    return this.animateurService.getAnimateur();
   }
 
 }
