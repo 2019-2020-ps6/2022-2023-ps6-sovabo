bbca5862c7f1ca80ae9ae7440aac95434781b222
Julien ORIOL <julien@MBP-de-Julien.lan>
2023-05-10 20:06:51 +0200

    feat: creation de toute la page creationQuiz Ref #30

--- front-end/src/app/quizz/creer-quizz/creer-quizz.component.ts
+++ front-end/src/app/quizz/creer-quizz/creer-quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {JeuxCouleursService} from "../../../service/jeux-couleurs.service";
 
 @Component({
   selector: 'app-creer-quizz',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./creer-quizz.component.scss']
 })
 export class CreerQuizzComponent {
+
+  contrasteTroubleEnable: boolean = this.jeuxCouleursService.getVisionAttentionStatus();
+
+  constructor(private jeuxCouleursService: JeuxCouleursService) {
+  }
 
 }
