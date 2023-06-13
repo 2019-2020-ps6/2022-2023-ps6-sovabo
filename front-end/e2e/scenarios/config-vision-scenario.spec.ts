a4ad1f54c0c753cfefb764ce6fbc12446b346611
JulienD <julien.didier@etu.unice.fr>
2023-06-13 10:09:01 +0200

    check changement background btn DEUTERANOMALIE Ref.#98

--- front-end/e2e/scenarios/config-vision-scenario.spec.ts
+++ front-end/e2e/scenarios/config-vision-scenario.spec.ts
@@ -1,25 +1,41 @@
 import { test, expect } from '@playwright/test';
 import {configVisionUrl, testUrl} from 'e2e/e2e.config';
 import { ConfigVisionFixture } from 'src/app/config/config-vision/config-vision.fixture'
+import {timeout} from "rxjs";
 
 // https://playwright.dev/docs/locators
 test.describe('configVision page display', () => {
   test('Jeux De Couleur Test', async ({ page }) => {
-    await test.step('Default Theme', await() => {
+    await test.step('Default Theme', async () => {
+      let cvf = new ConfigVisionFixture(page);
+      await page.goto(configVisionUrl);
+      // Sélecteur CSS pour le bouton
+      const bouton = '#DEUTERANOMALIE>button';
+
+      // Vérification du style initial de l'élément cible
+      var styleInitial = await page.$eval(bouton, function (el) {
+        return getComputedStyle(el).backgroundImage
+      });
+      expect(styleInitial).toBe('linear-gradient(261.58deg, rgba(178, 0, 0, 0.5) -77.61%, rgba(124, 101, 169, 0.5) 143.35%)');
 
     });
-    let cvf = new ConfigVisionFixture(page);
-    await page.goto(configVisionUrl);
-    // Sélecteur CSS pour le bouton
 
-    const bouton = '#DEUTERANOMALIE>button';
+    await test.step('Changes to DEUTERANOMALIE', async () => {
+      let cvf = new ConfigVisionFixture(page);
+      await page.goto(configVisionUrl);
+      // Sélecteur CSS pour le bouton
+      const bouton = '#DEUTERANOMALIE>button';
 
-    // Vérification du style initial de l'élément cible
-    var styleInitial = await page.$eval(bouton, function (el) {
-      return getComputedStyle(el).backgroundImage
-    });
-    expect(styleInitial).toBe('linear-gradient(261.58deg, rgba(178, 0, 0, 0.5) -77.61%, rgba(124, 101, 169, 0.5) 143.35%)');
+      //on clique sur le bouton
+      await page.click(bouton, {timeout: 2000});
 
-    await page.close();
+      var styleDeuteranomalie = await page.$eval(bouton, function (el){
+        return getComputedStyle(el).backgroundColor
+      });
+      expect(styleDeuteranomalie).toBe(cvf.getDeuteranomalieColorBtn());
+
+      await page.close();
+    })
+
   });
 });
--- front-end/src/app/config/config-vision/config-vision.fixture.ts
+++ front-end/src/app/config/config-vision/config-vision.fixture.ts
@@ -2,6 +2,21 @@ import { E2EComponentFixture } from "e2e/e2e-component.fixture";
 
 export class ConfigVisionFixture extends E2EComponentFixture {
 
+  hexToRGB(hex:string, alpha:number|null) {
+    var r = parseInt(hex.slice(1, 3), 16),
+      g = parseInt(hex.slice(3, 5), 16),
+      b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+      return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+      return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+  }
+
+  getDeuteranomalieColorBtn(){
+    return this.hexToRGB('#805ad0',null);
+  }
   getDefaultBackgroundReturn(){
     return 'rgba(0, 0, 0, 0)';
   }
