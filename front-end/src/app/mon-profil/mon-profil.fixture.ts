fe2534f0fee04480e83a29d8fc769b7340549679
victor dedieu <louis.victordedieu@gmail.com>
2023-06-13 10:24:29 +0200

    feat: implémentation des fixtures profils page Ref #101

---
+++ front-end/src/app/mon-profil/mon-profil.fixture.ts
@@ -0,0 +1,76 @@
+import {E2EComponentFixture} from "e2e/e2e-component.fixture";
+import {Locator} from "@playwright/test";
+
+export class MonProfilFixture extends E2EComponentFixture {
+  getTitle() {
+    return this.page.getByRole('heading', { name: 'PolyQuiz' });
+  }
+
+  getCreerButton() {
+    return this.page.getByRole('button', { name: 'CREER PROFIL' });
+  }
+
+  getBtnDeleteUser() {
+    return this.page.getByRole('button', { name: 'SUPPRIMER PROFIL' });
+  }
+
+  getBtnRetour() {
+    return this.page.getByRole('button', { name: 'RETOUR' });
+  }
+
+  async getInputNom() {
+    return this.page.$('input[type="text"]');
+  }
+
+  getBtnValider() {
+    return this.page.getByRole('button', { name: 'Valider' });
+  }
+
+  async getCardFromUser(name: string) {
+    const card = await this.page.locator('.card:has(h5:has-text("Test"))');
+    if (!card) throw new Error(`No card found with name ${name}`);
+    return card;
+  }
+
+  async getCardCreatingUser() {
+      return await this.page.locator('.creating-card');
+  }
+
+  async getChoisirButtonFromUser(card: Locator) {
+    const choisirBtn = await card.locator('button:has-text("Choisir")');
+    if (!choisirBtn) throw new Error(`No choisir button found on card ${card}`);
+    return choisirBtn;
+  }
+
+  async getBtnAnnulerCreation() {
+    return this.page.getByRole('button', { name: 'Annuler' });
+  }
+
+  async getUserSVG(name?:string) {
+    if (!name) return this.page.locator('.user-svg');
+    return this.page.locator('.card:has(h5:has-text("Test")) .user-svg')
+  }
+
+  async modifyAvatar() {
+    if (await this.page.isVisible('.selectAvatar')) {
+      await this.page.locator('.selectAvatar .avatar-image').nth(0).click();
+    }
+    else if (await this.page.isVisible('.modifyAvatar')) {
+      await this.page.locator('.modifyAvatar .avatar-image').nth(1).click();
+    }
+  }
+
+  async closeModalAvatar() {
+    if (await this.page.isVisible('.selectAvatar')) {
+      await this.page.locator('.selectAvatar .close-button').click();
+    }
+    else if (await this.page.isVisible('.modifyAvatar')) {
+      await this.page.locator('.modifyAvatar .close-button').click();
+    }
+  }
+
+  async getBtnAnnulerSuppression() {
+    return this.page.getByRole('button', { name: 'ANNULER' });
+  }
+
+}
